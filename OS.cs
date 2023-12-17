7816521b1c13f3f62037e7823713dd0a74d4c6ba
nyarkus_ <116504973+nyarkus@users.noreply.github.com>
2023-12-17 17:20:11 +0300

    1.2.0

--- OS.cs
+++ OS.cs
@@ -3,5 +3,14 @@
     public abstract class ConsoleOS
     {
         public abstract ConsoleOSInfo osinfo { get; }
+        public ConsoleOS() { }
+    }
+    public abstract class OSInstaller
+    {
+        public abstract void Install();
+        public abstract void Uninstall();
+        public abstract void FastInstall();
+        public abstract void FastDelete();
+        public OSInstaller() { }
     }
 }
